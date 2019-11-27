076f4e072424a412273e3d562a6438918d56dce6
Bert54 <matt.bert@live.fr>
2019-11-27 13:25:48 +0100

    First Commit (Q1 & Q2 & Q3)

--- src/TestClass.java
+++ src/TestClass.java
@@ -6,8 +6,24 @@ public class TestClass {
         System.out.println("Fractional Packing (Inferior Limit) : " + BinPacking.getInstance().fractionalPacking("src/res/DSJC125.5.txt") + " boxes");
         System.out.println("First Fit Packing : " + BinPacking.getInstance().firstFitDecreasingPack("src/res/DSJC125.5.txt") + " boxes");
         System.out.println("Best Fit Packing : " + BinPacking.getInstance().bestFitDecreasingPacking("src/res/DSJC125.5.txt") + " boxes");
+        System.out.println();
 
+        System.out.println("Results with 250 Objects (Objects size are randomly selected between " + BinPacking.ITEMMINSIZE + " and " + BinPacking.ITEMMAXSIZE + ")");
+        System.out.println("Fractional Packing (Inferior Limit) : " + BinPacking.getInstance().fractionalPacking("src/res/DSJC250.5.txt") + " boxes");
+        System.out.println("First Fit Packing : " + BinPacking.getInstance().firstFitDecreasingPack("src/res/DSJC250.5.txt") + " boxes");
+        System.out.println("Best Fit Packing : " + BinPacking.getInstance().bestFitDecreasingPacking("src/res/DSJC250.5.txt") + " boxes");
+        System.out.println();
 
+        System.out.println("Results with 500 Objects (Objects size are randomly selected between " + BinPacking.ITEMMINSIZE + " and " + BinPacking.ITEMMAXSIZE + ")");
+        System.out.println("Fractional Packing (Inferior Limit) : " + BinPacking.getInstance().fractionalPacking("src/res/DSJC500.5.txt") + " boxes");
+        System.out.println("First Fit Packing : " + BinPacking.getInstance().firstFitDecreasingPack("src/res/DSJC500.5.txt") + " boxes");
+        System.out.println("Best Fit Packing : " + BinPacking.getInstance().bestFitDecreasingPacking("src/res/DSJC500.5.txt") + " boxes");
+        System.out.println();
+
+        System.out.println("Results with 1000 Objects (Objects size are randomly selected between " + BinPacking.ITEMMINSIZE + " and " + BinPacking.ITEMMAXSIZE + ")");
+        System.out.println("Fractional Packing (Inferior Limit) : " + BinPacking.getInstance().fractionalPacking("src/res/DSJC1000.5.txt") + " boxes");
+        System.out.println("First Fit Packing : " + BinPacking.getInstance().firstFitDecreasingPack("src/res/DSJC1000.5.txt") + " boxes");
+        System.out.println("Best Fit Packing : " + BinPacking.getInstance().bestFitDecreasingPacking("src/res/DSJC1000.5.txt") + " boxes");
     }
 
 }
